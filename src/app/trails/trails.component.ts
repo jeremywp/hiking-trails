b216fb7d80c3333ec41642ac6b489f7b23d915e6
jeremywp <dr.jpratt@gmail.com>
2019-01-22 08:57:12 -0700

    angular material

---
+++ src/app/trails/trails.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trails',
+  templateUrl: './trails.component.html',
+  styleUrls: ['./trails.component.scss']
+})
+export class TrailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
