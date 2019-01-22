0ceca856dc81b18eb55962485182ca01b2e83eef
jeremywp <dr.jpratt@gmail.com>
2019-01-22 09:11:41 -0700

    Minimize app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TrailsComponent } from './trails/trails.component';
-import {MatFormFieldModule, MatInputModule} from "@angular/material";
+import {MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule} from "@angular/material";
 
 @NgModule({
   declarations: [
@@ -14,8 +14,10 @@ import {MatFormFieldModule, MatInputModule} from "@angular/material";
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MatCardModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
