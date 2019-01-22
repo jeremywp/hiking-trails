c48fbcadaf3940f12e907ce6e1d3e752ef481d10
jeremywp <dr.jpratt@gmail.com>
2019-01-22 09:06:01 -0700

    trails routing/zip code input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TrailsComponent } from './trails/trails.component';
+import {MatFormFieldModule, MatInputModule} from "@angular/material";
 
 @NgModule({
   declarations: [
@@ -12,7 +13,9 @@ import { TrailsComponent } from './trails/trails.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
