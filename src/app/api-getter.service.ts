233a7949b2fa5a7547d25b75809127951caa5ca6
Caleb Clayson <clayson.caleb@gmail.com>
2019-01-24 10:38:19 -0700

    made a data service

--- src/app/api-getter.service.ts
+++ src/app/api-getter.service.ts
@@ -14,7 +14,7 @@ export class ApiGetterService {
   }
 
   getWeather(lat, long) {
-    return this.http.get(`http://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${long}&cnt=5&APPID=810e3f85922b175841b0f7afdc1a2bbb`);
+    return this.http.get(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&$lon=${long}&cnt=5&APPID=741e467fcbe05d7f347015f01d1aab2f`);
   }
 
 }
