39f7bddafbc5f9b3691bba312de1488215522395
Caleb Clayson <clayson.caleb@gmail.com>
2019-01-25 10:27:12 -0700

    added some placeholder content to trails of user and added its route

--- src/app/trails-of-user/trails-of-user.component.ts
+++ src/app/trails-of-user/trails-of-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiGetterService } from '../api-getter.service';
 
 @Component({
   selector: 'app-trails-of-user',
@@ -7,11 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrailsOfUserComponent implements OnInit {
 
-  users;
+  usid = 103909;
+  users = [
+    {
+      id: 102903,
+      displayName: 'some guy'
+    },
+    {
+      id: 102940,
+      displayName: 'some other guy'
+    },
+    {
+      id: 103909,
+      displayName: 'the correct guy'
+    }
+  ];
+  currentUser;
 
-  constructor() { }
+  constructor(private apiGetter: ApiGetterService) { }
 
   ngOnInit() {
+    this.currentUser = this.findCurrentUser();
+    console.log(this.currentUser);
+  }
+
+  findCurrentUser() {
+    for(let i = 0; i < this.users.length; i++) {
+      if(this.users[i].id == this.usid) {
+        return this.users[i];
+      }
+    }
   }
 
 }
