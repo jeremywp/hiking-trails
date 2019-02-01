ddb34d2fba9cabd97e827b4e6cdb54c4a8be1074
Casey Randall <42552122+gospel02@users.noreply.github.com>
2019-02-01 10:26:41 -0700

    updates to trail of user page

--- src/app/trails-of-user/trails-of-user.component.ts
+++ src/app/trails-of-user/trails-of-user.component.ts
@@ -3,6 +3,8 @@ import { ApiGetterService } from '../api-getter.service';
 import {UserTrailsService} from "./user-trails.service";
 import {Trail} from "../trail";
 import {Observable} from "rxjs";
+import { User } from 'firebase';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 
 @Component({
@@ -11,34 +13,29 @@ import {Observable} from "rxjs";
   styleUrls: ['./trails-of-user.component.scss']
 })
 export class TrailsOfUserComponent implements OnInit {
-
-  usid = 103909;
-  users = [
-    {
-      id: 102903,
-      displayName: 'some guy'
-    },
-    {
-      id: 102940,
-      displayName: 'some other guy'
-    },
-    {
-      id: 103909,
-      displayName: 'the correct guy'
-    }
-  ];
+ 
+  
+  usid = 0;
+  users = [];
   currentUser;
   private completedTrails$: Observable<Trail>;
   private interestedTrails$: Observable<Trail>;
+  user: User;
   
 
 
   constructor(private apiGetter: ApiGetterService,
-              private userTrailsService: UserTrailsService) { }
+              private userTrailsService: UserTrailsService,
+              private afAuth: AngularFireAuth) { }
 
   ngOnInit() {
+    this.afAuth.authState.subscribe(user => {
+      this.user = user;
+      console.log(this.user);
+
+    });
     this.currentUser = this.findCurrentUser();
-    console.log(this.currentUser);
+    
     this.completedTrails$ = this.userTrailsService.getCompletedTrailsObservable();
     this.interestedTrails$ = this.userTrailsService.getInterestedTrailsObservable();
     this.userTrailsService.getUser();
@@ -48,8 +45,12 @@ export class TrailsOfUserComponent implements OnInit {
     for(let i = 0; i < this.users.length; i++) {
       if(this.users[i].id == this.usid) {
         return this.users[i];
+        
+        
       }
+      
     }
+    
   }
 
 
